6af5ecc131ba7f78eb70ff025e45f89aa7f39e07
Sang Goo Kang <sanggoo@doordash.com>
2017-09-13 08:41:57 -0700

    added documentation

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -11,6 +11,15 @@ logger = logging.getLogger("auto_deepnet")
 logger.setLevel(logging.DEBUG)
 logging.basicConfig(format='%s(levelname)s:%(message)s', level=logging.DEBUG)
 
+'''
+function: save_file
+inputs:
+    - file_path: string pathname to save data to
+    - data: data to save to disk in any picklable format
+    - overwrite: whether a preexisting file should be overwritten
+description:
+    helper function to save any data to disk
+'''
 def save_file(file_path, data, overwrite=False):
     logger.info("Attempting to save data to {} ...".format(file_path))
     dir_name, file_name = os.path.split(file_path)
@@ -34,14 +43,25 @@ def save_file(file_path, data, overwrite=False):
         return True
     logger.error("Something failed. Returning...")
 
+'''
+function: load_file
+inputs:
+    - file_path: string pathname to load data from
+description:
+    helper function to load any pickled data from disk
+'''
 def load_file(file_path):
     logger.info("Attempting to load data from {} ...".format(file_path))
     if not os.path.isfile(file_path):
         logger.error("File {} does not exist. Returning...".format(file_path))
         return
-    logger.info("Opening file {} to read...".format(file_path))
+    logger.info("Opening file {} to read and unpickle...".format(file_path))
     with open(file_path, "rb") as f:
-        data = pickle.load(f)
+        try:
+            data = pickle.load(f)
+        except Exception as e:
+            logger.exception("Failed with Error {0}".format(e))
+            return
         logger.info("Successful")
         return data
     logger.error("Something failed. Returning...")
